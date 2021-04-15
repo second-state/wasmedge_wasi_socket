434575b2a9721c4f709ddb53466d7589ed33c8a1
Yi Huang <yi@secondstate.io>
2021-04-15 09:26:23 +0000

    [Lib] Add function declarations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
 #[link(wasm_import_module = "ssvm")]
 extern "C" {
-    pub fn ssvm_sock_open(fd: u32, addr_family: u8, sock_type: u8) -> u32;
+    pub fn ssvm_sock_open(addr_family: u8, sock_type: u8) -> u32;
+    pub fn ssvm_sock_close(fd: u32);
+    pub fn ssvm_sock_bind(fd: u32, addr: *const u8, addr_len: u32);
+    pub fn ssvm_sock_recv(fd: u32, buf: *mut u8, buf_len: u32, flags: u16) -> u32;
+    pub fn ssvm_sock_recv_from(
+        fd: u32,
+        buf: *mut u8,
+        buf_len: u32,
+        addr: *const u8,
+        addr_len: u32,
+        flags: u16,
+    ) -> u32;
+    pub fn ssvm_sock_send(fd: u32, buf: *const u8, buf_len: u32, flags: u16) -> u32;
+    pub fn ssvm_sock_send_to(
+        fd: u32,
+        buf: *const u8,
+        buf_len: u32,
+        addr: *const u8,
+        addr_len: u32,
+        flags: u16,
+    ) -> u32;
+    pub fn ssvm_sock_shutdown(fd: u32, flags: u8);
 }
