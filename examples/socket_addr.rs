79a9a475520fb666a58dd78751061c580bb76287
Tricster <mediosrity@gmail.com>
2023-02-01 13:51:09 +0800

    [Lib] Implement ToSocketAddrs within WASI. (#77)
    * [Lib] Implement ToSocketAddrs within WASI.
    
    Signed-off-by: Tricster <mediosrity@gmail.com>
    
    * [Example] Add ToSocketAddrs example.
    
    Signed-off-by: Tricster <mediosrity@gmail.com>
    
    * [CI] Update workflows.
    
    Signed-off-by: Tricster <mediosrity@gmail.com>
    
    ---------
    
    Signed-off-by: Tricster <mediosrity@gmail.com>

---
+++ examples/socket_addr.rs
@@ -0,0 +1,37 @@
+use wasmedge_wasi_socket::{SocketAddr, ToSocketAddrs};
+
+fn main() -> std::io::Result<()> {
+    // ip + port
+    let mut addr = ("127.0.0.1", 3000).to_socket_addrs()?;
+    assert_eq!(addr.next(), Some(SocketAddr::from(([127, 0, 0, 1], 3000))));
+    assert!(addr.next().is_none());
+
+    // ip
+    let mut addr = ("127.0.0.1:3000").to_socket_addrs()?;
+    assert_eq!(addr.next(), Some(SocketAddr::from(([127, 0, 0, 1], 3000))));
+    assert!(addr.next().is_none());
+
+    // from slice
+    let addr1 = SocketAddr::from(([0, 0, 0, 0], 80));
+    let addr2 = SocketAddr::from(([127, 0, 0, 1], 443));
+    let addrs = vec![addr1, addr2];
+
+    let mut addrs_iter = (&addrs[..]).to_socket_addrs().unwrap();
+
+    assert_eq!(Some(addr1), addrs_iter.next());
+    assert_eq!(Some(addr2), addrs_iter.next());
+    assert!(addrs_iter.next().is_none());
+
+    // missing port
+    let err = "127.0.0.1".to_socket_addrs().unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+
+    // unable to resolve
+    assert!("foo:443".to_socket_addrs().is_err());
+
+    // dns
+    let mut addr = ("localhost:3000").to_socket_addrs()?;
+    assert_eq!(addr.next(), Some(SocketAddr::from(([127, 0, 0, 1], 3000))));
+
+    Ok(())
+}
