866b864d0b9f413bda0fe1b2abac51dea34b2668
csh <458761603@qq.com>
2023-06-26 18:32:38 +0800

    [Lib] compatible with version 0.12.1 and earlier ver.

--- examples/tcp_stream.rs
+++ examples/tcp_stream.rs
@@ -6,6 +6,7 @@ fn main() -> std::io::Result<()> {
     println!("connect to 127.0.0.1:{}", port);
     let mut stream = TcpStream::connect(format!("127.0.0.1:{}", port))?;
     println!("local address {}", stream.local_addr().unwrap());
+    println!("peer address {}", stream.peer_addr().unwrap());
     println!("sending hello message...");
     stream.write(b"hello")?;
     stream.shutdown(Shutdown::Both)?;
