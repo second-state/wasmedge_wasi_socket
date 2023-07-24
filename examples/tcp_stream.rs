66df13d81e84214fb8fdd5e5b5b253074cf120d0
zzz <458761603@qq.com>
2023-07-24 23:53:43 +0800

    [Lib] fix `Socket::get_peer` & `Socket::get_local` (#88)
    * [Lib] fix `Socket::get_peer` & `Socket::get_local`
    
    * [Misc] Bump to 0.5.1
    
    * [CI] add 0.12
    
    * [Lib] compatible with version 0.12.1 and earlier ver.

--- examples/tcp_stream.rs
+++ examples/tcp_stream.rs
@@ -5,6 +5,8 @@ fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT").unwrap_or("1234".to_string());
     println!("connect to 127.0.0.1:{}", port);
     let mut stream = TcpStream::connect(format!("127.0.0.1:{}", port))?;
+    println!("local address {}", stream.local_addr().unwrap());
+    println!("peer address {}", stream.peer_addr().unwrap());
     println!("sending hello message...");
     stream.write(b"hello")?;
     stream.shutdown(Shutdown::Both)?;
